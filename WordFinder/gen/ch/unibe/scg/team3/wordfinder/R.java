bbd94a9a036cb6162914fe83f97e28a19727951f
Nils Schnabel <nils.schnabel@students.unibe.ch>
2013-10-12 00:33:03 +0200

    HomeActivity fixes

--- WordFinder/gen/ch/unibe/scg/team3/wordfinder/R.java
+++ WordFinder/gen/ch/unibe/scg/team3/wordfinder/R.java
@@ -24,13 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080002;
-        public static final int button3=0x7f080003;
-        public static final int button4=0x7f080004;
-        public static final int button5=0x7f080005;
-        public static final int linearLayout1=0x7f080000;
+        public static final int action_settings=0x7f050006;
+        public static final int friends_button=0x7f050005;
+        public static final int linearLayout1=0x7f050001;
+        public static final int preferences_button=0x7f050002;
+        public static final int saved_games_button=0x7f050003;
+        public static final int start_game_button=0x7f050000;
+        public static final int statistics_button=0x7f050004;
     }
     public static final class layout {
         public static final int activity_end_game=0x7f030000;
@@ -38,21 +38,21 @@ public final class R {
         public static final int activity_home=0x7f030002;
     }
     public static final class menu {
-        public static final int end_game=0x7f070000;
-        public static final int game=0x7f070001;
-        public static final int home=0x7f070002;
+        public static final int end_game=0x7f080000;
+        public static final int game=0x7f080001;
+        public static final int home=0x7f080002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int friends_button=0x7f050009;
-        public static final int hello_world=0x7f050002;
-        public static final int preferences_button=0x7f050006;
-        public static final int saved_games_button=0x7f050007;
-        public static final int start_game_button=0x7f050005;
-        public static final int statistics_button=0x7f050008;
-        public static final int title_activity_end_game=0x7f050004;
-        public static final int title_activity_game=0x7f050003;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int friends_button=0x7f060009;
+        public static final int hello_world=0x7f060002;
+        public static final int preferences_button=0x7f060006;
+        public static final int saved_games_button=0x7f060007;
+        public static final int start_game_button=0x7f060005;
+        public static final int statistics_button=0x7f060008;
+        public static final int title_activity_end_game=0x7f060004;
+        public static final int title_activity_game=0x7f060003;
     }
     public static final class style {
         /** 
@@ -76,10 +76,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
