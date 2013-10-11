27560f0bd4e7d78dceaa34d95f11252857591757
Nils Schnabel <nils.schnabel@students.unibe.ch>
2013-10-12 00:23:20 +0200

    HomeActivity Design and Intent on Startbutton

--- WordFinder/src/ch/unibe/scg/team3/wordfinder/HomeActivity.java
+++ WordFinder/src/ch/unibe/scg/team3/wordfinder/HomeActivity.java
@@ -3,6 +3,7 @@ package ch.unibe.scg.team3.wordfinder;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class HomeActivity extends Activity {
@@ -11,6 +12,10 @@ public class HomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);        
+    }
+    protected void startGame(){
+    	Intent intent = new Intent(this, GameActivity.class);
+    	startActivity(intent);
     }
 
 
