d8962e3bd300a7285888dbe20171331449e38d5a
Nils Schnabel <nils.schnabel@students.unibe.ch>
2013-10-11 15:54:41 +0200

    test

--- WordFinder/src/ch/unibe/scg/team3/wordfinder/HomeActivity.java
+++ WordFinder/src/ch/unibe/scg/team3/wordfinder/HomeActivity.java
@@ -10,6 +10,8 @@ public class HomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        System.out.print("hallo");
+        System.out.println("hello");
     }
 
 
