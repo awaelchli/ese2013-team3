2b6e47bd0bf8341018556e5ca226868ed673303b
Nils Schnabel <nils.schnabel@students.unibe.ch>
2013-10-13 04:39:11 +0200

    Added SQLite Helperclass

---
+++ WordFinder/src/ch/unibe/scg/team3/localDatabase/MySQLiteHelper.java
@@ -0,0 +1,31 @@
+package ch.unibe.scg.team3.localDatabase;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MySQLiteHelper extends SQLiteOpenHelper {
+	private static final String DATABASE_NAME ="localDatabase.db";
+	private static final int DATABASE_VERSION=1;
+
+	public MySQLiteHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		
+	}
+
+
+	@Override
+	public void onCreate(SQLiteDatabase arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
