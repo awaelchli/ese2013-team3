32018cafab2bf92828609db7491a078d5d43ca06
awaelchli <adrian.waelchli@students.unibe.ch>
2013-10-14 15:17:26 +0200

    testing boardgenerator

---
+++ WordFinder/src/ch/unibe/scg/team3/board/RandomBoardGeneratorTest.java
@@ -0,0 +1,33 @@
+package ch.unibe.scg.team3.board;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import token.*;
+
+import ch.unibe.jexample.JExample;
+
+/**
+ * 
+ * @author adrian
+ * 
+ */
+@RunWith(JExample.class)
+public class RandomBoardGeneratorTest {
+
+	@Test
+	public void testBoardWithSize1(){
+		RandomBoardGenerator r = new RandomBoardGenerator(1);
+		r.generate();
+		
+		Board board = r.getBoard();
+		
+		IToken tok = board.getToken(0, 0);
+		IToken nul = NullToken.getInstance();
+		
+		assertFalse(tok.getLetter() == nul.getLetter());
+	}
+	
+}
