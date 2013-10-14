e582d4a487fa7110a358da18380b591fc052a188
awaelchli <adrian.waelchli@students.unibe.ch>
2013-10-14 18:50:58 +0200

    more testing of random boardgen

--- WordFinder/src/ch/unibe/scg/team3/board/RandomBoardGeneratorTest.java
+++ WordFinder/src/ch/unibe/scg/team3/board/RandomBoardGeneratorTest.java
@@ -1,6 +1,6 @@
 package ch.unibe.scg.team3.board;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,6 +28,26 @@ public class RandomBoardGeneratorTest {
 		IToken nul = NullToken.getInstance();
 		
 		assertFalse(tok.getLetter() == nul.getLetter());
+	}
+	
+	@Test 
+	public void testBoardNoNullToken(){
+		RandomBoardGenerator r = new RandomBoardGenerator(60);
+		r.generate();
+		
+		Board board = r.getBoard();
+		IToken nul = NullToken.getInstance();
+		
+		String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		
+		for(int i = 0; i < board.getSize(); i++){
+			for(int j = 0; j < board.getSize(); j++){
+				
+				IToken tok = board.getToken(i, j);
+				assertFalse(tok.equals(nul));
+				assertTrue(alphabet.contains(tok.toString()));
+			}
+		}
 	}
 	
 }
