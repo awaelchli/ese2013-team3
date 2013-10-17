a64bf212b39073a56a385652287d9239e14a3b8c
nfaerber <nicco@students.unibe.ch>
2013-10-17 16:23:22 +0200

    int[] to Point

--- WordFinder/src/ch/unibe/scg/team3/wordfinder/GridActivity.java
+++ WordFinder/src/ch/unibe/scg/team3/wordfinder/GridActivity.java
@@ -7,6 +7,7 @@ import java.util.List;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.Menu;
@@ -32,12 +33,12 @@ import ch.unibe.scg.team3.game.GameManager;
 		The list elements order is the order in that the corresponding
 		fields were walked.
 	**/
-	List<int[]> walked_coordinates;
+	List<Point> walked_coordinates;
 	/**
 	  	Adapt this value in the onCreate() method if your fingers are too fat!
 	 */
 	int finger_padding;
-	HashMap<String, int[]> hmap;
+	HashMap<String, Point> hmap;
 	GameManager manager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,18 +52,18 @@ import ch.unibe.scg.team3.game.GameManager;
             	boolean isInList = false;
             	
             	switch (event.getAction()) {
-            	case MotionEvent.ACTION_DOWN:
-            		walked = new ArrayList<View>();
-            		walked_coordinates = new ArrayList<int[]>();
-            	case MotionEvent.ACTION_MOVE:
-            		break;
-            	case MotionEvent.ACTION_UP:
-            		for (int i=0;i<walked.size();i++) {
-            			walked.get(i).setBackgroundDrawable(getResources().getDrawable(R.drawable.buttonlayout));
-            		}
-            		return true;
-            	default:
-            		return false;
+            		case MotionEvent.ACTION_DOWN:
+            			walked = new ArrayList<View>();
+            			walked_coordinates = new ArrayList<Point>();
+            		case MotionEvent.ACTION_MOVE:
+            			break;
+            		case MotionEvent.ACTION_UP:
+            			for (int i=0;i<walked.size();i++) {
+            				walked.get(i).setBackgroundDrawable(getResources().getDrawable(R.drawable.buttonlayout));
+            			}
+            			return true;
+            		default:
+            			return false;
             	}
             	
                 TableLayout layout = (TableLayout)v;
@@ -110,25 +111,25 @@ import ch.unibe.scg.team3.game.GameManager;
     public void createHashMap() {
     	// Maps view's id to view's location on board
     	// int[] = {row,column} whereas {0,0} is the top left corner and {5,5} the bottom right corner
-        hmap = new HashMap<String, int[]>();
-        hmap.put("button1", new int[] {0,0}); hmap.put("button2", new int[] {0,1});
-        hmap.put("button3", new int[] {0,2}); hmap.put("button4", new int[] {0,3});
-        hmap.put("button5", new int[] {0,4}); hmap.put("button6", new int[] {0,5});
-        hmap.put("button7", new int[] {1,0}); hmap.put("button8", new int[] {1,1});
-        hmap.put("button9", new int[] {1,2}); hmap.put("button10", new int[] {1,3});
-        hmap.put("button11", new int[] {1,4}); hmap.put("button12", new int[] {1,5});
-        hmap.put("button13", new int[] {2,0}); hmap.put("button14", new int[] {2,1});
-        hmap.put("button15", new int[] {2,2}); hmap.put("button16", new int[] {2,3});
-        hmap.put("button17", new int[] {2,4}); hmap.put("button18", new int[] {2,5});
-        hmap.put("button19", new int[] {3,0}); hmap.put("button20", new int[] {3,1});
-        hmap.put("button21", new int[] {3,2}); hmap.put("button22", new int[] {3,3});
-        hmap.put("button23", new int[] {3,4}); hmap.put("button24", new int[] {3,5});
-        hmap.put("button25", new int[] {4,0}); hmap.put("button26", new int[] {4,1});
-        hmap.put("button27", new int[] {4,2}); hmap.put("button28", new int[] {4,3});
-        hmap.put("button29", new int[] {4,4}); hmap.put("button30", new int[] {4,5});
-        hmap.put("button31", new int[] {5,0}); hmap.put("button32", new int[] {5,1});
-        hmap.put("button33", new int[] {5,2}); hmap.put("button34", new int[] {5,3});
-        hmap.put("button35", new int[] {5,4}); hmap.put("button36", new int[] {5,5});
+        hmap = new HashMap<String, Point>();
+        hmap.put("button1", new Point(0,0)); hmap.put("button2", new Point(1,0));
+        hmap.put("button3", new Point(2,0)); hmap.put("button4", new Point(3,0));
+        hmap.put("button5", new Point(4,0)); hmap.put("button6", new Point(5,0));
+        hmap.put("button7", new Point(0,1)); hmap.put("button8", new Point(1,1));
+        hmap.put("button9", new Point(2,1)); hmap.put("button10", new Point(3,1));
+        hmap.put("button11", new Point(4,1)); hmap.put("button12", new Point(5,1));
+        hmap.put("button13", new Point(0,2)); hmap.put("button14", new Point(1,2));
+        hmap.put("button15", new Point(2,2)); hmap.put("button16", new Point(3,2));
+        hmap.put("button17", new Point(4,2)); hmap.put("button18", new Point(5,2));
+        hmap.put("button19", new Point(0,3)); hmap.put("button20", new Point(1,3));
+        hmap.put("button21", new Point(2,3)); hmap.put("button22", new Point(3,3));
+        hmap.put("button23", new Point(4,3)); hmap.put("button24", new Point(5,3));
+        hmap.put("button25", new Point(0,4)); hmap.put("button26", new Point(1,4));
+        hmap.put("button27", new Point(2,4)); hmap.put("button28", new Point(3,4));
+        hmap.put("button29", new Point(4,4)); hmap.put("button30", new Point(5,4));
+        hmap.put("button31", new Point(0,5)); hmap.put("button32", new Point(1,5));
+        hmap.put("button33", new Point(2,5)); hmap.put("button34", new Point(3,5));
+        hmap.put("button35", new Point(4,5)); hmap.put("button36", new Point(5,5));
     }
 
     @Override
@@ -154,4 +155,3 @@ import ch.unibe.scg.team3.game.GameManager;
     	finish();
 	}
 }
-
