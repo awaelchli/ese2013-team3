e44b83886de242a870a4e2979248054978959bbe
awaelchli <adrian.waelchli@students.unibe.ch>
2013-11-05 17:31:06 +0100

    new boardgenerator for board creation for board string input

---
+++ WordFinder/src/ch/unibe/scg/team3/board/RawBoardBuilder.java
@@ -0,0 +1,41 @@
+package ch.unibe.scg.team3.board;
+
+import ch.unibe.scg.team3.token.Token;
+
+public class RawBoardBuilder extends AbstractBoardGenerator {
+
+	private String raw;
+
+	public RawBoardBuilder(String board) {
+		super(board.length());
+		raw = board;
+	}
+
+	@Override
+	protected void generate() {
+		double side = Math.sqrt(raw.length());
+
+		if (side % 0 == 0) {
+			fillBoard((int) side);
+		}
+	}
+
+	private void fillBoard(int size) {
+		
+		int index = 0;
+		
+		for (int i = 0; i < size; i++) {
+			for(int j = 0; j < size; j++){
+				
+				char letter = raw.charAt(index);
+				int value = meter.getValue(letter);
+				
+				Token tok = new Token(letter, value, i, j);
+				board.setToken(tok);
+				
+				index++;
+			}
+		}
+	}
+
+}
