a82d18ebb57ef4ff2822bd6bba75f6066b4bd430
Nils Schnabel <nils.schnabel@students.unibe.ch>
2013-11-12 16:05:09 +0100

    Databasechanges

--- WordFinder/src/ch/unibe/scg/team3/game/Game.java
+++ WordFinder/src/ch/unibe/scg/team3/game/Game.java
@@ -59,8 +59,10 @@ public class Game extends AbstractGame {
 	}
 
 	private void generateBoard(int boardSize) {
-		PrimitiveDBBoardGenerator gen = new PrimitiveDBBoardGenerator(boardSize, wordlistHandler,
+		PrimitiveRecursiveDBBoardGenerator gen = new PrimitiveRecursiveDBBoardGenerator(boardSize, wordlistHandler,
 				DEFAULT_MIN_WORDS_TO_FIND);
+		gen.generate();
+		board = gen.getBoard();
 
 	}
 
