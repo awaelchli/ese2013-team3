ee74adf228442a8b40ad04cd8f2d6ec6b26f72ef
Nils Schnabel <nils.schnabel@students.unibe.ch>
2013-11-27 00:22:00 +0100

    added request creation and changed layout hope it works on every device

--- WordFinder/src/ch/unibe/scg/team3/wordfinder/FriendsActivity.java
+++ WordFinder/src/ch/unibe/scg/team3/wordfinder/FriendsActivity.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.app.Activity;
@@ -18,86 +19,108 @@ import android.content.Context;
 public class FriendsActivity extends Activity {
 
 	private List<ParseUser> friends;
+	private List<ParseUser> requests;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_friends);
 		friends = new ArrayList<ParseUser>();
+		requests = new ArrayList<ParseUser>();
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
 
-		ParseUser me = ParseUser.getCurrentUser();
-		if (me != null) {
-			String id = me.getObjectId();
+		ParseQueryAdapter<ParseObject> adapter = new CustomParseQueryAdapter(this,
+				new ParseQueryAdapter.QueryFactory<ParseObject>() {
 
-			ParseQuery<ParseObject> friendship = ParseQuery.getQuery("Friendship");
-			friendship.whereEqualTo("user_id", id);
+					@Override
+					public ParseQuery<ParseObject> create() {
+						ParseUser me = ParseUser.getCurrentUser();
+						if (me != null) {
+							String id = me.getObjectId();
 
-			friendship.findInBackground(new FindCallback<ParseObject>() {
-				public void done(List<ParseObject> friendships, ParseException e) {
-					if (e == null) {
-						setupFriendList(friendships);
-					} else {
-						// TODO: handle this, prompt user
-						e.printStackTrace();
+							ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(
+									"Friendship");
+							query.whereEqualTo("user_id", id);
+							return query;
+						}
+						return null;
 					}
-				}
-			});
-		}
-	}
 
-	private void updateAdapter() {
-
-		ArrayAdapter<ParseUser> adapter = new ArrayAdapter<ParseUser>(this,
-				R.layout.friend_list_item, friends) {
-
-			@Override
-			public View getView(int position, View convertView, ViewGroup parent) {
-
-				LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-				final View element = inflater.inflate(R.layout.friend_list_item, parent, false);
-
-				ParseUser friend = getItem(position);
-				TextView name = (TextView) element.findViewById(R.id.friend_username);
-				name.setText(friend.getUsername());
-
-				element.setId(position);
-				return element;
-			}
-
-		};
+				});
 
 		ListView list = (ListView) findViewById(R.id.friends_list);
 		list.setAdapter(adapter);
-
 	}
 
-	private void setupFriendList(List<ParseObject> friendships) {
-
-		for (ParseObject friendship : friendships) {
-			String friend_id = friendship.getString("friend_id");
-
-			ParseQuery<ParseUser> query = ParseUser.getQuery();
-			query.whereEqualTo("objectId", friend_id);
-
-			query.findInBackground(new FindCallback<ParseUser>() {
-
-				@Override
-				public void done(List<ParseUser> result, ParseException arg1) {
-					friends.addAll(result);
-					updateAdapter();
-				}
-
-			});
-		}
-	}
-
+	
 	public void addFriend(View view) {
+		EditText email_view = (EditText) findViewById(R.id.emailToAdd);
+		String email = email_view.getText().toString();
+
+		ParseUser me = ParseUser.getCurrentUser();
+
+		final String myId = me.getObjectId();
+
+		final ParseQuery<ParseUser> user = ParseQuery.getUserQuery();
+		user.whereEqualTo("email", email);
+
+		user.findInBackground(new FindCallback<ParseUser>() {
+			public void done(List<ParseUser> requests, ParseException e) {
+				if (e == null) {
+					String userId = "";
+						try {
+							userId = (String) user.getFirst().getObjectId();
+						} catch (ParseException e1) {
+							e1.printStackTrace();
+						}
+					
+					ParseObject request = new ParseObject("Request");
+					request.put("initiator_id", myId);
+					request.put("subject_id", userId);
+					request.saveInBackground();
+
+				} else {
+					// TODO: handle this, prompt user
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
+class CustomParseQueryAdapter extends ParseQueryAdapter<ParseObject> {
+
+	public CustomParseQueryAdapter(Context context,
+			com.parse.ParseQueryAdapter.QueryFactory<ParseObject> queryFactory) {
+		super(context, queryFactory);
+	}
+
+	@Override
+	public View getItemView(ParseObject friendship, View v, ViewGroup parent) {
+		if (v == null) {
+			v = View.inflate(getContext(), R.layout.friend_list_item, null);
+		}
+		super.getItemView(friendship, v, parent);
+
+		TextView name = (TextView) v.findViewById(R.id.friend_username);
+
+		String friendId = friendship.getString("friend_id");
+		ParseQuery<ParseUser> query = ParseUser.getQuery();
+
+		try {
+			ParseUser friend = query.get(friendId);
+
+			name.setText(friend.getUsername());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return v;
+	}
+
+}
+
