ece69cf7808710d350a062c79ab1cb5c8ae3595e
Phyranja <vita@Charon.(none)>
2013-11-26 18:45:50 +0100

    added some errorhandling to SignUpActivity

--- WordFinder/src/ch/unibe/scg/team3/wordfinder/SignUpActivity.java
+++ WordFinder/src/ch/unibe/scg/team3/wordfinder/SignUpActivity.java
@@ -11,6 +11,8 @@ import ch.unibe.scg.team3.localDatabase.UserHandler;
 import ch.unibe.scg.team3.user.MainUser;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -50,13 +52,33 @@ public class SignUpActivity extends Activity {
 		     muser.setEmail(email);
 		     muser.setUserId(user.getObjectId().toString());
 		     uh.setUser(muser);
-		     
+		     finish();
 		    } else {
+		    	//TODO error handling
+		    	int code = e.getCode();
+		    	String message="someting is wrong";
+		    	if(code==ParseException.CONNECTION_FAILED){message="You need internet connection to sign up";}
+		    	if(code==ParseException.USERNAME_MISSING){message="You need to provide a username";}
+		    	if(code==ParseException.USERNAME_TAKEN){message="This username is allready taken";}
+		    	if(code==ParseException.EMAIL_MISSING || code==ParseException.EMAIL_TAKEN || code==ParseException.INVALID_EMAIL_ADDRESS){
+		    		message="This email address is either allready assigned to an account or is not a valid address";
+		    	}
+		    	
+		    	AlertDialog.Builder alert = new AlertDialog.Builder(SignUpActivity.this);
+
+				alert.setTitle("Error");
+				alert.setMessage(message);
+
+				alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int whichButton) {
+					}
+				});
+
+				alert.show();
 		      
 		    }
 		  }
 		});
-		finish();
 	}
 	
 
