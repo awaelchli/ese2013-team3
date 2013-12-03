634a9eb232586b02998c291a63d293ac1c221a93
awaelchli <adrian.waelchli@students.unibe.ch>
2013-12-03 15:13:16 +0100

    implemented accept/reject friend request but not fully completed. beware: code is ugly and will cause brain damage!

--- WordFinder/src/ch/unibe/scg/team3/sharingService/Friendship.java
+++ WordFinder/src/ch/unibe/scg/team3/sharingService/Friendship.java
@@ -4,26 +4,33 @@ public class Friendship {
 	private String objectId;
 	private String userId;
 	private String friendId;
+
 	public Friendship(String objectId, String userId, String friendId) {
 		this.objectId = objectId;
 		this.userId = userId;
 		this.friendId = friendId;
 	}
+
 	public String getObjectId() {
 		return objectId;
 	}
+
 	public void setObjectId(String objectId) {
 		this.objectId = objectId;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getFriendId() {
 		return friendId;
 	}
+
 	public void setFriendId(String friendId) {
 		this.friendId = friendId;
 	}
